2860947f964c4759962f0d88888a43bac495735b
ytspotplayer <leonhartpaulo@gmail.com>
2026-03-29 12:15:14 -0300

    Delete nonuser35-jam directory

--- nonuser35-jam/tampermonkey/INICIAR_YT_SYNC_HOST.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "try { & '%~dp0start_yt_sync_remote_host.ps1' } catch { Write-Host ''; Write-Host 'ERRO:' -ForegroundColor Red; Write-Host $_.Exception.Message -ForegroundColor Red }"
-
-echo.
-pause
-
-endlocal
--- nonuser35-jam/tampermonkey/start_yt_sync_remote_host.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-
-powershell -ExecutionPolicy Bypass -File "%~dp0start_yt_sync_remote_host.ps1"
-
-endlocal
